1682b600df295049e9f1e0b82bc8be8d77982aba
ImMasterSam <sam9630147@gmail.com>
2025-03-11 22:25:29 +0800

    Add Python File

---
+++ main.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+
+path = "./dataset/dtA/test_data.csv"
+df = pd.read_csv(path)
+print(df)
---
+++ models/KNN.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+class KNNClassifier:
+
+    def __init__(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def predict(self) -> float:
+        pass
