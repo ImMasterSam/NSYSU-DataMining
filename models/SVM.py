c380e24aef06cafec18f1f0c06de3ac2f96a8ffd
ImMasterSam <sam9630147@gmail.com>
2025-03-13 17:54:02 +0800

    Add Other Models

---
+++ models/SVM.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+class SVMClassifier:
+
+    def __init__(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def score(self) -> float:
+        pass
