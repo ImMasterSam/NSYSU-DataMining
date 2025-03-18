e484f5c16372cf6508b397150fb29275292d670a
ImMasterSam <sam9630147@gmail.com>
2025-03-18 11:49:04 +0800

    KNN: Add Norm Distance

--- models/KNN.py
+++ models/KNN.py
@@ -3,16 +3,17 @@ import pandas as pd
 
 class KNNClassifier:
 
-    def __init__(self, k: int, normalize: bool = True):
+    def __init__(self, k: int, normalize: bool = True, normDistance: int = 2):
         self.k = k
-        self.normal = normalize
+        self.normalize = normalize
+        self.normDis = normDistance
         pass
 
     def fit(self, x_train: pd.DataFrame, y_train: pd.DataFrame):
         self.trainDatas = x_train
         self.trainLabels = y_train
 
-        if self.normal:
+        if self.normalize:
             self.mean = self.trainDatas.mean()
             self.std = self.trainDatas.std()
             self.trainDatas = (self.trainDatas - self.mean) / self.std
@@ -21,21 +22,21 @@ class KNNClassifier:
 
         res = []
 
-        if self.normal:
+        if self.normalize:
             x_test = (x_test - self.mean) / self.std
         
-        for i in range(x_test.shape[0]):                      # 測試資料行數
-            te_num = x_test.loc[i].to_numpy()                 # 轉成numpy
+        for i in range(x_test.shape[0]):                                        # 測試資料行數
+            te_num = x_test.loc[i].to_numpy()                                   # 轉成numpy
             distance_between = []
-            for j in range(self.trainDatas.shape[0]):               # 訓練資料行數
-                df_num = self.trainDatas.loc[j].to_numpy()          # 轉乘numpy
+            for j in range(self.trainDatas.shape[0]):                           # 訓練資料行數
+                df_num = self.trainDatas.loc[j].to_numpy()                      # 轉乘numpy
  
-                distance = 0                               	# 初始化距離
+                distance = 0                               	                    # 初始化距離
  
-                for k in range(x_test.shape[1]):            # 到outcome前
-                    distance += (te_num[k] - df_num[k])**2  # 算距離
+                for k in range(x_test.shape[1]):                                # 到outcome前
+                    distance += (abs(te_num[k] - df_num[k]))**self.normDis      # 算距離
 
-                distance_between.append([distance**(1/2), self.trainLabels[j]])  # append開方距離以及outcome
+                distance_between.append([distance**(1/self.normDis), self.trainLabels[j]])  # append開方距離以及outcome
             distance_between.sort()
 
             # Moore Majority Voting Algorithm
@@ -47,7 +48,7 @@ class KNNClassifier:
                 else:
                     cnt -= 1
                 
-                if cnt < 0:
+                if cnt <= 0:
                     major = distance_between[i][1]
             
             res.append(major)
@@ -61,11 +62,10 @@ class KNNClassifier:
         right = 0
 
         for i in range(len(y_predict)):
-            print(y_test[i], y_predict[i])
             if y_test[i] == y_predict[i]:
                 total += 1
                 right += 1
             else:
                 total += 1
 
-        print((right/total)*100,"%")
+        print("Score:", (right/total)*100, "%")
