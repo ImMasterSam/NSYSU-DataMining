c6fd7a9c16588411e1c35b95e7492516b51ee581
ImMasterSam <sam9630147@gmail.com>
2025-04-12 23:02:42 +0800

    Fix Linear SVM model

--- models/SVM.py
+++ models/SVM.py
@@ -3,48 +3,52 @@ import pandas as pd
 
 class SVMClassifier:
 
-    def __init__(self,learning_rate=0.001, n_iters = 1000):
+    def __init__(self, learning_rate=0.001, n_iters = 1000, normalize: bool = True):
         self.lr = learning_rate
         self.n_iters = n_iters
+        self.normalize = normalize
         self.w = None #公式的權重
         self.b = None #偏差
         pass
 
     def fit(self, x_train: pd.DataFrame, y_train: pd.DataFrame):
-        x_train = x_train.astype(float)
-        y_train = y_train.astype(int)
+        x_train = x_train.astype(float).to_numpy()
+        y_train = y_train.astype(int).to_numpy()
 
-        x_train = x_train.to_numpy()
-        y_train = y_train.to_numpy()
+        if self.normalize:
+            self.mean = x_train.mean()
+            self.std = x_train.std()
+            x_train = (x_train - self.mean) / self.std
 
-        n_samples , n_features = x_train.shape #sameple 為行 feature為列
-        self.w = np.zeros(n_features) #因為svm最後要表達為f(x) = w_1*x1 + w_2*x2 + ...... + w_8*x8 + b 所以初始化向量的特徵
+        n_samples, n_features = x_train.shape   # sameple 為行 feature為列
+        self.w = np.random.randn(n_features)    # 因為svm最後要表達為f(x) = w_1*x1 + w_2*x2 + ...... + w_8*x8 + b 所以初始化向量的特徵
         self.b = 0
 
-        y_t = np.where(y_train <= 0 , -1 ,1) #這個公式在將outcome傳換成-1 1因為svm是用-1 1做分類的 y_train <= 0 就是-1 else 0
-        
-        for _ in range(self.n_iters):#跑迴圈 不斷分類
-            for idx , x_i in enumerate(x_train):
-                condition = y_t[idx] * (np.dot(x_i,self.w) + self.b) >= 1 #梯度下降 分類對邊 >= 1 分類對邊但離中間太近0~1 分錯邊
-                if condition:
-                    continue
-                else:
-                    self.w -= self.lr * (-np.dot(x_i,y_t[idx]))
-                    self.b -= self.lr * y_t[idx]
-        pass
+        y_t = np.where(y_train <= 0 , -1 ,1)    # 這個公式在將outcome傳換成-1 1因為svm是用-1 1做分類的 y_train <= 0 就是-1 else 1
+
+        for _ in range(self.n_iters):           # 跑迴圈 不斷學習
+            for idx, x_i in enumerate(x_train):
+                margin = y_t[idx] * (np.dot(x_i, self.w) + self.b)
+                if margin < 1:
+                    self.w += self.lr * y_t[idx] * x_i
+                    self.b += self.lr * y_t[idx]
 
     def predict(self , x_test: pd.DataFrame):
-        x_test = np.array(x_test)
+        x_test = x_test.astype(float).to_numpy()
+
+        if self.normalize:
+            x_test = (x_test - self.mean) / self.std
 
         #分類 (x.w + b) 內積 + bias
-        classification = np.sign(np.dot(x_test,self.w)+self.b)
-        return classification
+        classification = np.dot(x_test, self.w)+self.b
+        print(classification)
+        return np.where(classification <= 0, 0, 1)
 
-    def score(self,x_test:pd.DataFrame, y_test: pd.Series) -> float:
+    def score(self, x_test:pd.DataFrame, y_test: pd.Series) -> float:
         predictions = self.predict(x_test)
-        y_true = np.where(y_test <= 0 , -1 , 1)
+        print(predictions)
 
-        accuracy = np.mean(predictions == y_true)
+        accuracy = np.mean(y_test == predictions)
         print(f"Score: {accuracy * 100: .2f} %")
         return accuracy
         
