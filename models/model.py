b269878289d560ce5e71de549fb40bf367de3e8a
ImMasterSam <sam9630147@gmail.com>
2025-04-14 10:32:59 +0800

    Fix Typo :)

--- models/model.py
+++ models/model.py
@@ -14,7 +14,7 @@ class Classifier:
     def predict(self, x_test: pd.DataFrame):
         pass
 
-    def analysis(self, x_test: pd.DataFrame, y_test : pd.Series) -> float:
+    def score(self, x_test: pd.DataFrame, y_test : pd.Series) -> float:
         ''' 輸出準確度 Accuracy '''
         y_predict = self.predict(x_test)
         accuracy = np.mean(y_predict == y_test.to_numpy().astype(int))
